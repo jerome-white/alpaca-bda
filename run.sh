48342f27c47e4bfaf88cf4f69be1375bf2efe260
Jerome White <jerome@thewex.net>
2024-02-13 12:33:02 +0000

    Enhance run script
    * Add new Stan pipeline processing
    * Exports for logging and importing

--- run.sh
+++ run.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-ALPACA_ROOT=$HOME/Public/misc/alpaca_eval
+export PYTHONPATH=.
+export PYTHONLOGLEVEL=info
 
+ALPACA_ROOT=$HOME/alpaca_eval
+
+(cd $ALPACA_ROOT && git pull origin main) 1>&2
 python compile-results.py --results $ALPACA_ROOT/results \
-    | python prompt-encoder.py --dump prompts.json \
-    | python dedup-order.py
+    | python prompt-encoder.py --record prompts.json \
+    | python dedup-order.py \
+    | python make-data.py \
+    | python codify-models.py --record models.csv \
+    | python to-stan.py
