8ce5d949f28dad3fc24e9ea3ebb7d7270f147279
Jerome White <jerome@thewex.net>
2024-02-16 10:28:41 +0000

    Data aggretation models specific

---
+++ models/bradley-terry/aggregate-data.py
@@ -0,0 +1,37 @@
+import sys
+import csv
+from argparse import ArgumentParser
+
+import pandas as pd
+
+def each(fp):
+    usecols = [
+        'generator_1',
+        'generator_2',
+    ]
+    scores = dict(zip(usecols, (1, 0)))
+
+    usecols.append('preference')
+    df = (pd
+          .read_csv(fp, usecols=usecols)
+          .dropna())
+
+    for i in df.itertuples(index=False):
+        result = { x: getattr(i, x) for x in scores }
+
+        for (k, v) in result.items():
+            if i.preference == v:
+                result['y'] = scores[k]
+                break
+        else:
+            raise ValueError('Unexpected preference: {i.preference}')
+
+        yield result
+
+if __name__ == '__main__':
+    writer = None
+    for row in each(sys.stdin):
+        if writer is None:
+            writer = csv.DictWriter(sys.stdout, fieldnames=row)
+            writer.writeheader()
+        writer.writerow(row)
--- run.sh
+++ run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ROOT=`git rev-parse --show-toplevel`
+
 ALPACA_EVAL=alpaca_eval
 ALPACA_GIT=$ROOT/$ALPACA_EVAL
+STAN_MODEL=$ROOT/models/bradley-terry
 
 export PYTHONPATH=$ROOT
 export PYTHONLOGLEVEL=info
@@ -15,5 +17,6 @@ fi
 
 python compile-results.py --results $ALPACA_GIT/results \
     | python prompt-encoder.py --record prompts.json \
-    | python aggregate-data.py \
-    | python stan-encoder.py --record models.csv
+    | python $STAN_MODEL/aggregate-data.py \
+    | python stan-encoder.py --record $STAN_MODEL/models.csv \
+	     > $STAN_MODEL/data.json
