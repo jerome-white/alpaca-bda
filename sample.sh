2f6d46834b2889d28588ce74c6122a1e6bcfb4e9
Jerome White <jerome@thewex.net>
2024-02-17 11:56:48 +0000

    Specify samples per chain instead of overall samples

--- sample.sh
+++ sample.sh
@@ -2,22 +2,18 @@
 
 ROOT=`git rev-parse --show-toplevel`
 STAN_MODEL=$ROOT/models/bradley-terry
+N=2000
 
 _output=$STAN_MODEL/output
 _summary=$STAN_MODEL/summary.csv
-_samples=200000
-
-num_chains=`nproc`
-num_samples=$(bc -l <<< "$_samples / $num_chains" \
-		  | cut --delimiter='.' --fields=1
-	   )
+_workers=`nproc`
 
 mkdir $_output 2> /dev/null || rm --recursive --force $_output/*
 rm --force $_summary
 
 $STAN_MODEL/model \
-    sample num_samples=$num_samples num_chains=$num_chains \
+    sample num_samples=$N num_chains=$_workers \
     data file=$STAN_MODEL/data.json \
     output file=$_output/o.csv \
-    num_threads=$num_chains
+    num_threads=$_workers
 stansummary --csv_filename=$_summary $_output/*.csv
