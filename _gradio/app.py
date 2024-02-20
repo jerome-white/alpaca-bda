12ee2ea57a6f85dad2659786d6a646a565e17564
Jerome White <jerome@thewex.net>
2024-02-20 16:36:36 +0530

    Add HDI directly

--- _gradio/app.py
+++ _gradio/app.py
@@ -1,5 +1,8 @@
+import math
+import operator as op
 import itertools as it
 import functools as ft
+import collections as cl
 from pathlib import Path
 
 import pandas as pd
@@ -8,7 +11,26 @@ import seaborn as sns
 from datasets import load_dataset
 from scipy.special import expit
 
-from hdi import HDI, hdi
+HDI = cl.namedtuple('HDI', 'lower, upper')
+
+#
+# See https://cran.r-project.org/package=HDInterval
+#
+def hdi(values, ci=0.95):
+    values = sorted(filter(math.isfinite, values))
+    if not values:
+        raise ValueError('Empty data set')
+
+    n = len(values)
+    exclude = n - math.floor(n * ci)
+
+    left = it.islice(values, exclude)
+    right = it.islice(values, n - exclude, None)
+
+    diffs = ((x, y, y - x) for (x, y) in zip(left, right))
+    (*args, _) = min(diffs, key=op.itemgetter(-1))
+
+    return HDI(*args)
 
 #
 #
