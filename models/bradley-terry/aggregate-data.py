b2745ba3407b468d2f9573845c4b643a11718aaa
Jerome White <jerome@thewex.net>
2024-03-04 09:33:30 +0000

    Use Pandas for outcome encoding

--- models/bradley-terry/aggregate-data.py
+++ models/bradley-terry/aggregate-data.py
@@ -1,36 +1,18 @@
 import sys
-import csv
 
 import pandas as pd
 
-def each(fp):
-    usecols = [
-        'generator_1',
-        'generator_2',
-    ]
-    scores = dict(zip(usecols, (1, 0)))
-
-    usecols.append('preference')
-    df = (pd
-          .read_csv(fp, usecols=usecols)
-          .dropna())
-
-    for i in df.itertuples(index=False):
-        result = { x: getattr(i, x) for x in scores }
-
-        for (k, v) in result.items():
-            if i.preference == v:
-                result['y'] = scores[k]
-                break
-        else:
-            raise ValueError(f'Unexpected preference: {i.preference}')
-
-        yield result
-
 if __name__ == '__main__':
-    writer = None
-    for row in each(sys.stdin):
-        if writer is None:
-            writer = csv.DictWriter(sys.stdout, fieldnames=row)
-            writer.writeheader()
-        writer.writerow(row)
+    preference = 'preference'
+    (home, away) = map('generator_{}'.format, range(1, 3))
+
+    df = (pd
+          .read_csv(sys.stdin, usecols=[
+              home,
+              away,
+              preference,
+          ])
+          .dropna() # remove ties
+          .assign(y=lambda x: x[home] == x[preference])
+          .drop(columns=preference))
+    df.to_csv(sys.stdout, index=False)
