fba5349e7103622757417845d61f2a79ce38796d
Jerome White <jerome@thewex.net>
2024-03-14 09:20:41 +0000

    Assume data arrives from stdin

--- utils/push-to-hub.py
+++ utils/push-to-hub.py
@@ -1,6 +1,5 @@
 import sys
 import csv
-import gzip
 from pathlib import Path
 from argparse import ArgumentParser
 
@@ -13,15 +12,13 @@ class DataReader:
         'sample': int,
     }
     
-    def __init__(self, source):
-        self.source = source
+    def __init__(self, fp):
+        self.reader = csv.DictReader(fp)
 
     def __call__(self):
-        with gzip.open(self.source, mode='rt') as fp:
-            reader = csv.DictReader(fp)
-            for row in reader:
-                row.update(self.as_types(row))
-                yield row
+        for row in self.reader:
+            row.update(self.as_types(row))
+            yield row
 
     def as_types(self, row):
         for (k, v) in self._conversions.items():
@@ -29,10 +26,9 @@ class DataReader:
 
 if __name__ == '__main__':
     arguments = ArgumentParser()
-    arguments.add_argument('--source', type=Path)
     arguments.add_argument('--target', type=Path)
     args = arguments.parse_args()
 
-    reader = DataReader(args.source)
+    reader = DataReader(sys.stdin)
     dataset = Dataset.from_generator(reader)
     dataset.push_to_hub(str(args.target))
