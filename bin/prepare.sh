6555b102f4c78ee4526e25bfaf3bb40a6305f6d0
Jerome White <jerome@thewex.net>
2024-03-27 11:12:33 +0000

    Ensure Git does not write to stdout

--- bin/prepare.sh
+++ bin/prepare.sh
@@ -13,7 +13,7 @@ while getopts 'b:e:' option; do
     esac
 done
 
-git submodule update --remote --merge
+git submodule update --remote --merge 1>&2
 python $ROOT/utils/compile-results.py ${_baseline[@]} \
        --results $ROOT/alpaca_eval/results \
     | python $ROOT/utils/encode-results.py --save-encodings $_encodings
