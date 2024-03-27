00fbe56373ce865b53def61ac7e35b6f68e8d2b8
Jerome White <jerome@thewex.net>
2024-03-27 11:19:57 +0000

    Output now goes to stdout

--- bin/evaluate.sh
+++ bin/evaluate.sh
@@ -3,11 +3,10 @@
 ROOT=`git rev-parse --show-toplevel`
 
 _configs=$ROOT/alpaca_eval/src/alpaca_eval/models_configs
-while getopts 'm:e:o:p:' option; do
+while getopts 'm:e:p:' option; do
     case $option in
 	m) _model=$OPTARG ;;
 	e) _encodings=$OPTARG ;;
-	o) _output=$OPTARG ;;
 	p) _parameters=( ${_parameters[@]} --parameter $OPTARG ) ;;
         *)
             echo -e Unrecognized option \"$option\"
@@ -20,8 +19,6 @@ tmp=`mktemp`
 
 python $ROOT/utils/pretty-names.py --configs $_configs < $_encodings > $tmp
 python $ROOT/utils/aggregate-output.py --results $_model/output \
-    | python $ROOT/utils/unencode-results.py ${_parameters[@]} \
-	     --encodings $tmp \
-    | gzip --to-stdout --best > $_output
+    | python $ROOT/utils/unencode-results.py ${_parameters[@]} --encodings $tmp
 
 rm $tmp
