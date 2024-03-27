168616d1d2d5526de9946fbfa84dcea622405105
Jerome White <jerome@thewex.net>
2024-03-28 02:23:37 +0530

    Make room for Chatbot Arena parser

---
+++ utils/compile-results/arena.py
@@ -0,0 +1,52 @@
+# "https://drive.google.com/file/d/1Kpg6HD1QCrytCVT7FgRvZhY885TnmpEo/view?usp=sharing"
+
+import sys
+import csv
+import json
+from argparse import ArgumentParser
+from tempfile import NamedTemporaryFile
+from urllib.parse import urlparse, urlunparse
+
+import gdown
+
+def load(url, anony):
+    with NamedTemporaryFile(mode='w') as output:
+        source = gdown.download(
+            url,
+            output=output.name,
+            quiet=True,
+            fuzzy=True,
+        )
+        with open(output.name) as fp:
+            data = json.load(fp)
+
+    for i in data:
+        if i['anony'] or anony:
+            yield i
+
+def collect(battles, tie=''):
+    models = {
+        f'model_{x}': f'generator_{y}' for (x, y) in zip(('a', 'b'), (1, 2))
+    }
+
+    for b in battles:
+        row = { y: b[x] for (x, y) in models.items() }
+        row.update({
+            'annotator': b['judge'],
+            'preference':  b.get(b['winner'], tie),
+        })
+
+        yield row
+
+if __name__ == '__main__':
+    arguments = ArgumentParser()
+    arguments.add_argument('--source', type=urlparse)
+    arguments.add_argument('--ignore-anony', action='store_true')
+    args = arguments.parse_args()
+
+    writer = None
+    for row in collect(load(urlunparse(args.source), args.ignore_anony)):
+        if writer is None:
+            writer = csv.DictWriter(sys.stdout, fieldnames=row)
+            writer.writeheader()
+        writer.writerow(row)
