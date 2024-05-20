397c6c8a61826df6bbbef5fb5c20bb813a32776e
Jerome White <jerome@thewex.net>
2024-05-20 18:20:00 +0530

    Data preparation per framework

---
+++ frameworks/chatbot-arena/prepare.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+ROOT=`git rev-parse --show-toplevel`
+ARENA_DATA="https://storage.googleapis.com/arena_external_data/public"
+
+while getopts 'e:' option; do
+    case $option in
+	e) _encodings=$OPTARG ;;
+        *)
+            echo -e Unrecognized option \"$option\"
+            exit 1
+            ;;
+    esac
+done
+
+python $ROOT/utils/compile-results.py ${_baseline[@]} --source $ARENA_DATA \
+    | python $ROOT/utils/encode-results.py --save-encodings $_encodings
