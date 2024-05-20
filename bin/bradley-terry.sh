6e27dad498d3c54f18e006de062a1a0c22675858
Jerome White <jerome@thewex.net>
2024-05-20 18:31:58 +0530

    Main script capable of running all frameworks

--- bin/bradley-terry.sh
+++ bin/bradley-terry.sh
@@ -9,8 +9,20 @@ export PYTHONLOGLEVEL=info
 _codes=$SRC/codes.json
 _output=$SRC/results.csv.gz
 
-while getopts 'pseuh' option; do
+while getopts 'f:pseuh' option; do
     case $option in
+	f)
+	    _fm=$OPTARG
+	    case $OPTARG in
+		alpaca) ;;
+		chatbot-arena)
+		    _dataset=arena
+		    ;;
+		*)
+		    exit 1
+		    ;;
+	    esac
+	    ;;
 	p) _prepare=1 ;;
 	s) _sample=1 ;;
 	e) _evaluate=1 ;;
@@ -22,11 +34,15 @@ while getopts 'pseuh' option; do
     esac
 done
 
+if [ ! -d $ROOT/frameworks/$_fm ]; then
+    exit 1
+fi
+
 #
 #
 #
 if [ $_prepare ]; then
-    $ROOT/bin/prepare.sh -e $_codes -b gpt4_1106_preview \
+    $ROOT/frameworks/$_fm/prepare.sh -e $_codes \
 	| python $SRC/aggregate-data.py \
 	| python $SRC/stan-encoder.py > $SRC/data.json
 fi || exit 1
@@ -42,7 +58,7 @@ fi || exit 2
 #
 #
 if [ $_evaluate ]; then
-    $ROOT/bin/evaluate.sh -m $SRC -e $_codes -p alpha:model \
+    $ROOT/frameworks/$_fm/evaluate.sh -m $SRC -e $_codes -p alpha:model \
 	| gzip --to-stdout --best > $_output
 fi || exit 3
 
@@ -52,5 +68,5 @@ fi || exit 3
 if [ $_upload ]; then
     zcat $_output \
 	| python $ROOT/utils/push-to-hub.py \
-		 --target jerome-white/alpaca-bt-stan
+		 --target jerome-white/${_dataset}-bt-stan
 fi || exit 4
